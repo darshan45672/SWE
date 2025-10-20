9a80ef774e3858bc3d77e756f346e6d0c99453e1
darshan45672 <drshnbhandary@gmail.com>
2025-10-20 09:19:27 +0530

    fixes

--- backend/dist/controllers/issue.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-export declare const getIssueById: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const getIssuesByProjectId: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const createIssue: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const updateIssue: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const deleteIssue: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-//# sourceMappingURL=issue.d.ts.map
--- backend/dist/validators/issue.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const createIssueValidation: import("express-validator").ValidationChain[];
-export declare const updateIssueValidation: import("express-validator").ValidationChain[];
-export declare const getIssueValidation: import("express-validator").ValidationChain[];
-export declare const getIssuesByProjectValidation: import("express-validator").ValidationChain[];
-export declare const deleteIssueValidation: import("express-validator").ValidationChain[];
-//# sourceMappingURL=issue.d.ts.map
